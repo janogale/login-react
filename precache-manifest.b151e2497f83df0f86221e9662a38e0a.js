23c9e26e822328b4cc671118578e8ca136fb2d72
janogale <mukhtar.mahomed@gmail.com>
2019-11-14 11:44:44 +0300

    Updates

---
+++ precache-manifest.b151e2497f83df0f86221e9662a38e0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcfb6c4b786750df8b555cea6e52bb3",
+    "url": "/login-react/index.html"
+  },
+  {
+    "revision": "6babbca8f98a8d67ab70",
+    "url": "/login-react/static/css/main.86717a08.chunk.css"
+  },
+  {
+    "revision": "de75f175bab004bd3e67",
+    "url": "/login-react/static/js/2.59b2e0e0.chunk.js"
+  },
+  {
+    "revision": "6babbca8f98a8d67ab70",
+    "url": "/login-react/static/js/main.63808de4.chunk.js"
+  },
+  {
+    "revision": "c945e0348073dedde73a",
+    "url": "/login-react/static/js/runtime-main.86486b26.js"
+  }
+]);
